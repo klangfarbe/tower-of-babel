339411281704b2e565db4cadffc4667300cb5b46
Christian Mocek <christian.mocek@gmail.com>
2014-01-14 10:43:38 +0100

    Added explosion when destroying an element

--- game/Assets/Scripts/Game/Gameplay/DestroyActor.cs
+++ game/Assets/Scripts/Game/Gameplay/DestroyActor.cs
@@ -5,6 +5,8 @@ public class DestroyActor : MonoBehaviour {
 	public bool isWinningCondition = true;
 
 	public void destroy() {
+		GameObject prefab = (GameObject) Resources.Load("Explosion");
+		Instantiate(prefab, transform.position, prefab.transform.rotation);
 		gameObject.SetActive(false);
 		if(isWinningCondition)
 			GameObject.Find("Level").GetComponent<Conditions>().destroyRobot();
