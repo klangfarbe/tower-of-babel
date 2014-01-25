3e0235246ed58d6932e43f0165f574687da34f3a
Christian Mocek <christian.mocek@gmail.com>
2014-01-25 23:03:00 +0100

    Added exchanger behaviour

---
+++ game/Assets/Scripts/Game/Gameplay/Exchanger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Exchanger : Reflector {
+	public override void grabbed(GameObject by) {
+		MoveActor actor = by.GetComponent<MoveActor>();
+		Vector3 tmp = actor.transform.position;
+		actor.set(transform.position);
+		by.transform.RotateAround(by.transform.position, Vector3.up, 180);
+		transform.position = tmp;
+		GetComponent<DestroyActor>().destroy();
+	}
+}
