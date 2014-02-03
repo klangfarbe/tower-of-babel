d4657d9076bcc63eaf3d343a3e09d7eeb5dbf305
Christian Mocek <christian.mocek@gmail.com>
2014-02-03 22:19:22 +0100

    Reworked icons

--- game/Assets/Scripts/Game/Controls/GUIButtons.cs
+++ game/Assets/Scripts/Game/Controls/GUIButtons.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GUIButtons : MonoBehaviour {
+	public SwitchCamera cameraSelector;
+
 	public Texture texZapper;
 	public Texture texGrabber;
 	public Texture texPusher;
@@ -13,29 +15,44 @@ public class GUIButtons : MonoBehaviour {
 	public Texture texFire;
 	public GUISkin skin;
 
+	private float sWidth = 1024f;
+	private float sHeight = 768f;
+
 	void OnGUI() {
+		calculateAspectRatio();
+
+		float xFactor = Screen.width / sWidth;
+		float yFactor = Screen.height / sHeight;
+		GUIUtility.ScaleAroundPivot(new Vector2(xFactor, yFactor), Vector2.zero);
+
 		GUI.skin = skin;
-		GUILayout.BeginArea(new Rect(Screen.width - 720, Screen.height - 560, 200, 160));
+		GUILayout.BeginArea(new Rect(sWidth / 2 - 320, sHeight - 80, 640, 70));
 		GUILayout.BeginHorizontal();
-        GUILayout.Button(texUpDown);
-        GUILayout.Button(texForward);
+        GUILayout.Button(texGrabber);
+        GUILayout.Button(texPusher);
+        GUILayout.Button(texZapper);
         GUILayout.Button(texFire);
-        GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
         GUILayout.Button(texLeft);
+        GUILayout.Button(texForward);
         GUILayout.Button(texBack);
         GUILayout.Button(texRight);
+        GUILayout.Button(texUpDown);
         GUILayout.EndHorizontal();
         GUILayout.EndArea();
 	}
 
-	// Use this for initialization
-	void Start () {
+	// ------------------------------------------------------------------------
 
-	}
-
-	// Update is called once per frame
-	void Update () {
-
+	void calculateAspectRatio () {
+		// calculate aspect ratio
+		var aspect = (float)Screen.width / (float)Screen.height;
+		if(aspect >= 1.7f && aspect < 1.78f) {
+			sWidth = 1280;
+			sHeight = 720;
+		} else if(aspect >= 1.3f && aspect < 1.34f) {
+			sWidth = 1024;
+			sHeight = 768;
+		}
+		Debug.Log("Screen aspect ratio is " + aspect.ToString("F4") + " with size " + Screen.width + "x" + Screen.height);
 	}
 }
