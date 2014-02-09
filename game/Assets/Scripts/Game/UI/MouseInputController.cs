c81c1ae8f5d369942f486d5044749b87f4a819f7
Christian Mocek <christian.mocek@gmail.com>
2014-02-09 14:32:02 +0100

    Simplified mouse behaviour in map mode

--- game/Assets/Scripts/Game/UI/MouseInputController.cs
+++ game/Assets/Scripts/Game/UI/MouseInputController.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 using System.Collections;
 
 public class MouseInputController : BaseUIController {
-	private float mouseRotationSensitivity = 128;
 	private float mouseSensitivity = 0.2f;
-	private float mouseX = 0f;
-	private float mouseY = 0f;
-	private Vector2 lastMousePosition;
+	private bool panningMode = false;
+
+	private float scLeft;
+	private float scMiddle;
+	private float scRight;
+
+	void Awake() {
+		scLeft = 0;
+		scMiddle = Screen.width / 3;
+		scRight = (Screen.width / 3) * 2;
+	}
 
 	void Update () {
 		// Zoom in/out
@@ -17,28 +24,21 @@ public class MouseInputController : BaseUIController {
 			cameraController.zoom(0.5f);
 		}
 
-		// Pan with left MB
-		if(Input.GetMouseButton(0)) {
-			mouseX = -Input.GetAxis("Mouse X") * mouseSensitivity;
-			mouseY = -Input.GetAxis("Mouse Y") * mouseSensitivity;
-			cameraController.translateOverview(new Vector2(mouseX, mouseY));
-		}
-
-		// rotate with right MB
-		if(Input.GetMouseButtonDown(1)) {
-			lastMousePosition = Input.mousePosition;
-		}
-		else if(Input.GetMouseButton(1)) {
-			mouseX = Input.GetAxis("Mouse X");
-			float delta = Vector2.Distance(lastMousePosition, Input.mousePosition);
-			if(mouseX < 0f && delta > mouseRotationSensitivity) {
+		// if clicked in middle of the screen start panning,
+		// otherwise rotate overview
+		if(Input.GetMouseButtonDown(0)) {
+			panningMode = false;
+			if(Input.mousePosition.x > scRight) {
 				cameraController.rotateOverview(90);
-				lastMousePosition = Input.mousePosition;
-			}
-			if(mouseX > 0f && delta > mouseRotationSensitivity) {
+			} else if(Input.mousePosition.x < scMiddle) {
 				cameraController.rotateOverview(-90);
-				lastMousePosition = Input.mousePosition;
+			} else {
+				panningMode = true;
 			}
+		} else if(Input.GetMouseButton(0) && panningMode) {
+			float mouseX = -Input.GetAxis("Mouse X") * mouseSensitivity;
+			float mouseY = -Input.GetAxis("Mouse Y") * mouseSensitivity;
+			cameraController.translateOverview(new Vector2(mouseX, mouseY));
 		}
 	}
 }
