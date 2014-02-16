c0f2723418e432043e48cad482cb1ef9e468c807
Christian Mocek <christian.mocek@gmail.com>
2014-02-16 21:13:34 +0100

    Changed spotlights for cannons

--- game/Assets/Scripts/Game/Effects/FadingLight.cs
+++ game/Assets/Scripts/Game/Effects/FadingLight.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 using System.Collections;
 
 public class FadingLight : MonoBehaviour {
-	private float fadeSpeed = 1.5f;			// How fast the light fades between intensities.
-	private float highIntensity = 1.5f;		// The maximum intensity of the light whilst the alarm is on.
-	private float lowIntensity = 0.1f;	   // The minimum intensity of the light whilst the alarm is on.
-	private float changeMargin = 0.2f;	   // The margin within which the target intensity is changed.
+	private float fadeSpeed = 2f;			// How fast the light fades between intensities.
+	private float highIntensity = 4f;		// The maximum intensity of the light whilst the alarm is on.
+	private float lowIntensity = 0.0f;	   // The minimum intensity of the light whilst the alarm is on.
+	private float changeMargin = 0.1f;	   // The margin within which the target intensity is changed.
 	public bool fadeOn;					 // Whether or not the alarm is on.
 
 	private float targetIntensity;		  // The intensity that the light is aiming for currently.
